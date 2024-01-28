43a8b7950f681fde58b167943f97b9e0499a75fe
Shikhar Mall <shmall.21.2020@gmail.com>
2024-01-29 01:30:04 +0530

    some change in env

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/podcast");
-
 require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+if (process.env.DATABASE === "MONGODBATLAS") {
+  mongoose.connect(process.env.DATABASEURL);
+  mongoose.connection.on("error", (err) => {
+    console.log("Connection Failed");
+  });
+  mongoose.connection.on("connected", (connected) => {
+    console.log("Connected to MongoDB Atlas.");
+  });
+} else if (process.env.DATABASE === "MONGODBLOCAL") {
+  mongoose.connect("mongodb://127.0.0.1:27017/podcast");
+  mongoose.connection.on("error", (err) => {
+    console.log("Connection Failed");
+  });
+  mongoose.connection.on("connected", (connected) => {
+    console.log("Connected to MongoDB Atlas.");
+  });
+} else {
+  console.log("No proper ENV.");
+}
 
 const port = process.env.PORT || 5174;
 
-var express = require('express');
+var express = require("express");
 var app = express();
 
-const userRoute = require('./routes/userRoute');
+const userRoute = require("./routes/userRoute");
 
-app.use('/', userRoute);
+app.use("/", userRoute);
 
-app.listen(port,()=>{
-  console.log('Running at port: '+ port);
+app.listen(port, () => {
+  console.log("Running at port: " + port);
 });
